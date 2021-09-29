01352103954c20b309f239bf35559346e93fd657
piyushpp07 <59327328+piyushpp07@users.noreply.github.com>
2021-09-29 17:05:16 +0530

    Warning removed

--- App.js
+++ App.js
@@ -11,7 +11,17 @@ import { StateProvider } from './components/Context/StateProvider'
 import { AntDesign } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 const Stack = createStackNavigator();
+import { LogBox } from 'react-native';
+import _ from 'lodash';
 
+LogBox.ignoreLogs(['Warning:...']); // ignore specific logs
+LogBox.ignoreAllLogs(); // ignore all logs
+const _console = _.clone(console);
+console.warn = message => {
+if (message.indexOf('Setting a timer') <= -1) {
+   _console.warn(message);
+   }
+};
 
 class App extends React.Component {
 
